688f23cc451dda40c1a4ca1419b47ecb0919f560
DEVZZAME <92978598+DEVZZAME@users.noreply.github.com>
2023-02-27 17:43:43 +0900

    commit for Jenkins Testing

--- src/main/java/com/cicd/jenkins/controller/TestController.java
+++ src/main/java/com/cicd/jenkins/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.cicd.jenkins.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @GetMapping(value = "/test")
     public String test() {
         return "Hey Jenkins! i have pushed! do your job!";
     }
